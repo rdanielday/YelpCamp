50797cd1e00c6b807447a752c530f198e9fc1ebd
paintedwall <rdanielday@gmail.com>
2017-05-01 20:10:18 +0000

    Refactor app.js file, create new route files to ensure clarity of code.

---
+++ routes/comments.js
@@ -0,0 +1,50 @@
+var express    = require("express"),
+    router     = express.Router({mergeParams: true}),
+    Campground = require("../models/campground"),
+    Comment    = require("../models/comment"),
+    User       = require("../models/user");
+    
+// comments new
+
+router.get("/new", isLoggedIn, function(req, res) {
+    Campground.findById(req.params.id, function(err, campground) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("comments/new", {campground: campground});
+        }
+    });
+});
+
+// comments create
+
+router.post("/", isLoggedIn, function(req, res) {
+    // lookup campground using id
+    Campground.findById(req.params.id, function(err, campground) {
+        if (err) {
+            console.log(err);
+            res.redirect("/campgrounds");
+        } else {
+            Comment.create(req.body.comment, function(err, comment) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    campground.comments.push(comment);
+                    campground.save();
+                    res.redirect("/campgrounds/" + campground._id);
+                }
+            });
+        }
+    });
+});
+
+// iddleware
+
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect("/login");
+};
+
+module.exports = router;
